bf95f0c77456d7996afb31ee8773d9f0a204bdac
ead139 <ead139@scarletmail.rutgers.edu>
2017-07-22 12:56:58 -0400

    Server Up

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 (async () => {
-  const express = require('express');
+  const express =  require('express');
   const PORT = 8080 || process.env.PORT;
   const app = express();
   const exphbs = require(`express-handlebars`);
-  const db = require("./models");
-  const sequelize = require('sequelize');
+  const db =  require("./models");
+  const sequelize =  require('sequelize');
+  const bodyParser = require('body-parser');
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.text());
@@ -12,8 +13,8 @@
   app.use(express.static("./public"));
   app.engine('handlebars', exphbs({defaultLayout: 'main'}));
   app.set('view engine', 'handlebars');
-  require("./controllers/html-routes.js")(app);
-  require("./controllers/api-routes.js")(app);
+  // require("./controllers/html-routes.js")(app);
+  // require("./controllers/api-routes.js")(app);
   await db.sequelize.sync({ force: true });
   app.get(`/`, (req, res) => {
     res.send(`hello`)
@@ -22,7 +23,4 @@
       console.log("App listening on PORT " + PORT);
   });
  }
-//   catch (error) {
-//     return Promise.reject(new Error(400));
-// }
 )();
