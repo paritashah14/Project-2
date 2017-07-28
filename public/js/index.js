fa297216cf867dc8762cdb2dfec5ba3c9a53fbf0
paritashah14 <paritashah14@yahoo.com>
2017-07-27 21:26:38 -0400

    added home

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,19 @@
-function showPassword() {
 
-   var key_attr = $('#key').attr('type');
 
-   if(key_attr != 'text') {
+$(function() {
+    $('#login-form-link').click(function(e) {
+    	$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
 
-       $('.checkbox').addClass('show');
-        $('#key').attr('type', 'text');
-
-   } else {
-
-       $('.checkbox').removeClass('show');
-        $('#key').attr('type', 'password');
-
-   }
-
-}
+});
